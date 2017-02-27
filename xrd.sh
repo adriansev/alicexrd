2180f7f6fa99562d4da1a50377ca29a15f6c37b1
asevcenc <adrian.sevcenco@cern.ch>
2017-02-28 01:03:49 +0200

    startXRD function for using variables in the config file

--- xrd.sh
+++ xrd.sh
@@ -376,6 +376,55 @@ startMon() {
 }
 
 ######################################
+startXRDserv_help () {
+echo "This wrapper for starting of Xrootd services use the following _required_ variables defined in the configuration"
+echo "__XRD_INSTANCE_NAME= name of the xrootd instance - required"
+echo "__XRD_LOG= log file - required"
+echo "__XRD_PIDFILE= pid file - required"
+echo "__XRD_DEBUG= if defined enable debug mode - optional"
+echo "for detailed explanation of arguments taken by xrd and cmsd see:"
+echo "http://xrootd.org/doc/dev44/xrd_config.htm#_Toc454222279"
+}
+
+######################################
+get_name_extension () {
+local filename="${1##*/}"
+local extension="${1##*.}"
+echo "${filename%.*}" "${extension}"
+}
+
+######################################
+startXRDserv () {
+
+CFG="$1"
+
+## get __XRD_ server arguments from config file. (are ignored by the actual xrd/cmsd)
+eval $(sed -ne '/__XRD_/p' ${CFG})
+
+## make sure that they are defined
+[[ -z "${__XRD_INSTANCE_NAME}" || -z "${__XRD_LOG}" || -z "${__XRD_PIDFILE}" ]] && { help; exit 1;}
+
+## not matter how is enabled the debug mode means -d
+[[ -n "${__XRD_DEBUG}" ]] && __XRD_DEBUG="-d"
+
+## in order to make distinction between xrd and cmsd for log and pid file we add _xrd/_cmsd just before the extension
+local arr_log=($(filename "${__XRD_LOG}"))
+local arr_pid=($(filename "${__XRD_PIDFILE}"))
+
+local XRD_LOG="$(dirname "${__XRD_LOG}")/${arr_log[0]}_xrd.${arr_log[1]}"
+local CMSD_LOG="$(dirname "${__XRD_LOG}")/${arr_log[0]}_cmsd.${arr_log[1]}"
+
+local XRD_PID="$(dirname "${__XRD_PIDFILE}")/${arr_pid[0]}_xrd.${arr_pid[1]}"
+local CMSD_PID="$(dirname "${__XRD_PIDFILE}")/${arr_pid[0]}_cmsd.${arr_pid[1]}"
+
+local XRD_START="/usr/bin/xrootd -b ${__XRD_DEBUG} -n ${__XRD_INSTANCE_NAME} -l ${XRD_LOG} -s ${XRD_PID} -c ${CFG}"
+local CMSD_START="/usr/bin/cmsd -b ${__XRD_DEBUG} -n ${__XRD_INSTANCE_NAME} -l ${CMSD_LOG} -s ${CMSD_PID} -c ${CFG}"
+
+eval ${XRD_START}
+eval ${CMSD_START}
+}
+
+######################################
 killXRD() {
     echo -n "Stopping xrootd/cmsd: "
 
@@ -384,7 +433,6 @@ killXRD() {
     /bin/sleep 1
     /usr/bin/pkill -9 -u $USER xrootd
     /usr/bin/pkill -9 -u $USER cmsd
-    /usr/bin/pkill -f -u $USER XrdOlbMonPerf
     /usr/bin/pkill -f -u $USER mpxstats
 
     echo_passed;
