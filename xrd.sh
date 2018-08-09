80471d5b570b5ee524742e6aa7fc747daa8d3cdc
Adrian Sevcenco <adrian.sevcenco@cern.ch>
2018-08-09 14:14:51 +0300

    xrd.sh : add description of environment variables that can steer the xrd.sh script

--- xrd.sh
+++ xrd.sh
@@ -755,7 +755,12 @@ else
     echo " [-limits] generate limits file";
     echo "";
     echo "Environment variables:";
-    echo "  XRDSH_NOWARN_ASLIB do not warn xrd.sh is sourced"
+    echo "  XRDSH_NOWARN_ASLIB : if set (any value) do not warn xrd.sh is sourced"
+    echo "  XRDREADONLY : if set (any value) export path as not writable - server will not be elected for writes "
+    echo ">>>>>> XRDSH_NOAPMON : if set (any value) ApMon components WILL NOT BE USED"
+    echo "  XRDRUNDIR : location of admin,core,logs,logsbackup dirs; if not set it will be XRDSHDIR/run/"
+    echo "  XRDCONFDIR : location of system.cnf and XRDCF (default xrootd.xrootd.cf) conf files; if not set it will be XRDSHDIR/xrootd.conf/"
+    echo "  XRDCONF : main configuration file - usually (default) named system.cnf; if not set it will have the value of XRDCONFDIR/system.cnf"
 fi
 }
 
